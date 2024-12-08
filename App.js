17bba8bfe6d00af002af99525837220852e7b6a0
Sujit kumar khilar <sujitkhilar07@gmail.com>
2024-12-08 11:45:05 +0530

    install parcel

--- App.js
+++ App.js
@@ -17,9 +17,12 @@
    </div>
 </div> */}
 
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div",{id:"parent"},[
    React.createElement("div",{id:"child"},[
-      React.createElement("h1",{},"I am an h1 tag"),
+      React.createElement("h1",{},"jay shree ram"),
       React.createElement("h2",{},"I am an h2 tag")
    ]),
       React.createElement("div",{id:"child2"},[
---
+++ README.md
@@ -0,0 +1,20 @@
+#i am sujit khilar
+
+#percel
+-Dev Build
+-Local server
+-HMR = Hot module Replacement
+-file Watching Algorithm -written in c++
+-Caching - Faster Builds
+-Image Optimization
+-minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-differential Bundling - support older browser
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+- Different dev and prod bundles
